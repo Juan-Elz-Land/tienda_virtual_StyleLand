fada2bfee7dc2d4eaf5afb142c746c2370c5e240
Juan-Elz-Land <47773523@certus.edu.pe>
2025-06-25 00:19:57 -0500

    tienda virtual js

--- js/opciones_menu.js
+++ js/opciones_menu.js
@@ -6,7 +6,7 @@
     function Mostrar_Menu_Opciones(){
         let op = opciones.map(op => `<li> <a id="${op.id}" href="${op.Url}" class="${op.Class}">${op.Page}</a></li>`)
         const nav_menu = document.querySelector(".nav-menu")
-        let menu =  `<a href="/index.html"><img src="/tienda_virtual_StyleLand/img/logotipp_StyleLan.png" alt="logotipo StyleLand" width="150" height="70"></a>
+        let menu =  `<a href="/tienda_virtual_StyleLand/index.html"><img src="/tienda_virtual_StyleLand/img/logotipp_StyleLan.png" alt="logotipo StyleLand" width="150" height="70"></a>
             <ul class="lista_menu">
                ${op.join(' ')}               
             </ul>`
@@ -14,7 +14,7 @@
         
         const sub_menu = document.querySelector(".dropdown")
         let s_op = sub_opciones.map(s_op => `<li onmouseover="Mostrar_Producto_Categ('${s_op.Name}')"> <a href="${s_op.Url}"></a><i class="${s_op.Class}"></i><span> ${s_op.Name}</span></li>`)
-        sub_menu.innerHTML = `<a id="productos" href="/index.html">Productos</a>
+        sub_menu.innerHTML = `<a id="productos" href="/tienda_virtual_StyleLand/index.html">Productos</a>
                     <ul class="dropdown-menu">
                         ${s_op.join(' ')}
                     </ul>`
